1d9e67de6c958896cb679d00ec3b235197ed13ec
sanchousese <saniasutula@gmail.com>
2014-07-22 15:59:13 +0300

    Multitouch, demonstration from camera

---
+++ PhoneApp2/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Phone.Controls;
+using PhoneApp2.Resources;
+using Microsoft.Devices;
+using Microsoft.Devices.Sensors;
+using goniometer.view;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using goniometer.model;
+
+namespace PhoneApp2
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private DrawOnCanvas drawOnCanvas;
+        bool isCameraDemonstrate = true;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            drawOnCanvas = new DrawOnCanvas(MyCanvas);
+            Touch.FrameReported += new TouchFrameEventHandler(Touch_FrameReported);
+            viewfinderBrush.SetSource(new PhotoCamera(CameraType.Primary));
+        }
+
+        double[] preXArray = new double[10];
+        double[] preYArray = new double[10];
+
+
+        private void Touch_FrameReported(object sender, TouchFrameEventArgs e)
+        {
+            int pointsNumber = e.GetTouchPoints(MyCanvas).Count;
+            TouchPointCollection pointCollection = e.GetTouchPoints(MyCanvas);
+
+            MyCanvas.Children.Clear();
+
+            if (pointsNumber == 1)
+            {
+                Point point = pointCollection[0].Position;
+                drawOnCanvas.touching(point);
+                textAngle.Text = AngleCounter.calculateAngle(point, MyCanvas) + "'";
+            }
+            else
+            {
+                Point first = pointCollection[0].Position;
+                Point second = pointCollection[1].Position;
+                Point middle = new Point(MyCanvas.Width / 2, MyCanvas.Height);
+
+                drawOnCanvas.touching(first);
+                drawOnCanvas.touching(second);
+
+                textAngle.Text = AngleCounter.calculateAngle(first, middle, second) + "'";
+            }
+        }
+
+        private void CameraButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isCameraDemonstrate)
+                CameraLayout.Visibility = Visibility.Visible;
+            else
+                CameraLayout.Visibility = Visibility.Collapsed;
+
+            isCameraDemonstrate = !isCameraDemonstrate;
+
+        }
+
+    }
+}
---
+++ PhoneApp2/view/DrawOnCanvas.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Windows.UI;
+
+namespace goniometer.view
+{
+    class DrawOnCanvas
+    {
+
+        private Canvas canvas;
+        private double radiusPoint = 10;
+
+
+        public DrawOnCanvas(Canvas canvas)
+        {
+            this.canvas = canvas;
+        }
+
+
+        public void touching(System.Windows.Point point)
+        {
+            drawLineTo(point);
+
+            drawPoint(point);
+        }
+
+
+        private void drawPoint(Point point)
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Fill = new SolidColorBrush(Colors.Green);
+            ellipse.Width = radiusPoint;
+            ellipse.Height = radiusPoint;
+            Canvas.SetLeft(ellipse, point.X - radiusPoint / 2);
+            Canvas.SetTop(ellipse, point.Y - radiusPoint / 2);
+            canvas.Children.Add(ellipse);
+        }
+
+        private void drawLineTo(Point point)
+        {
+            Line line = new Line();
+            line.Stroke = new SolidColorBrush(Colors.Green);
+            line.X1 = point.X;
+            line.Y1 = point.Y;
+            line.X2 = canvas.Width / 2;
+            line.Y2 = canvas.Height;
+            canvas.Children.Add(line);
+        }
+
+    }
+}

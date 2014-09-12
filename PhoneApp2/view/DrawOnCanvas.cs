56c5a4610b15e977ac61a0887eff3dbbae412668
sanchousese <saniasutula1@gmail.com>
2014-09-12 13:06:06 +0300

    Styled

--- PhoneApp2/view/DrawOnCanvas.cs
+++ PhoneApp2/view/DrawOnCanvas.cs
@@ -35,7 +35,7 @@ namespace goniometer.view
         private static void drawPoint(Point point)
         {
             Ellipse ellipse = new Ellipse();
-            ellipse.Fill = new SolidColorBrush(Colors.Green);
+            ellipse.Fill = new SolidColorBrush(Colors.Red);
             ellipse.Width = radiusPoint;
             ellipse.Height = radiusPoint;
             Canvas.SetLeft(ellipse, point.X - radiusPoint / 2);
@@ -45,13 +45,33 @@ namespace goniometer.view
 
         private static void drawLineTo(Point point)
         {
+            point = findThirdPoint(point);
             Line line = new Line();
-            line.Stroke = new SolidColorBrush(Colors.Green);
+            line.Stroke = new SolidColorBrush(Colors.Red);
+            line.StrokeThickness = 5;
+            line.Opacity = 0.5;
             line.X1 = point.X;
             line.Y1 = point.Y;
             line.X2 = canvas.Width / 2;
             line.Y2 = canvas.Height;
             canvas.Children.Add(line);
+
+        }
+
+
+        private static Point findThirdPoint(Point c)
+        {
+            double lambda = findRatio(c);
+            Point point = new Point();
+            point.X = (c.X * (1 + lambda) - canvas.Width / 2) / lambda;
+            point.Y = (c.Y * (1 + lambda) - canvas.Height) / lambda;
+            return point;
+        }
+
+        private static double findRatio(Point c)
+        {
+            double length = Math.Sqrt(Math.Pow(c.X - canvas.Width / 2, 2) + Math.Pow(c.Y - canvas.Height, 2));
+            return length / canvas.Width;
         }
 
     }
